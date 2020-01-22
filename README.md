2faeed991b487800737a1d87f7cff93697418c85
guskma <49189620+guskma@users.noreply.github.com>
2020-01-22 18:49:35 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# LXD maintenance scripts
+
+Push and execute specified script into LXD container.
+
+## Usage
+
+1. create any script in `./scripts` directory.
+2. execute: `./launch.sh TASK`  
+  `TASK` is your script filename without extention
+3. output `lxd-tasks.log` in current directory.
---
+++ launch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+LOGFILE="lxd-tasks.log"
+LXDBASEDIR="/tmp"
+
+SCRIPTBASEDIR=$(cd $(dirname $0); pwd)
+TASK=$1
+
+if [[ $TASK == "" ]];
+then
+	echo "usage: ./launch.sh TASK"
+	exit 1
+fi
+
+script=${SCRIPTBASEDIR}/scripts/${TASK}.sh
+tmpscript="${LXDBASEDIR}/lxdmainte-${TASK}-`date +%Y%m%d%H%M%S`.sh"
+
+if [[ ! -f $script ]];
+then
+	echo "Not found: $script"
+	exit 1
+fi
+
+echo "***** [`date`]: EXEC: $TASK *****" | tee -a $LOGFILE
+
+containers=`lxc list -c ns --format csv | grep RUNNING | cut -d, -f1`
+
+for nm in $containers
+do
+	echo -n "  $nm ... " | tee -a $LOGFILE
+	lxc file push $script $nm$tmpscript
+	lxc exec $nm -- /bin/bash $tmpscript
+	( [[ $? == 0 ]] && echo "OK" || echo "NG" ) | tee -a $LOGFILE
+	lxc file delete $nm$tmpscript
+done
